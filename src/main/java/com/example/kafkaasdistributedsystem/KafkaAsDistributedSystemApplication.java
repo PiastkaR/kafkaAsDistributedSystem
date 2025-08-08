cf98c394d810bfd003f630ba7f509bcb197bba6b
piastkar <rafal.piastka@contractors.roche.com>
2025-08-08 14:28:08 +0200

    Initial commit

---
+++ src/main/java/com/example/kafkaasdistributedsystem/KafkaAsDistributedSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaasdistributedsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAsDistributedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAsDistributedSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkaasdistributedsystem/KafkaAsDistributedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaasdistributedsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAsDistributedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
